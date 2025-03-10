c6e6beed307bd20088b1ac10ef867bba92738a48
amsteinkruger <amsteinkruger@gmail.com>
2025-03-10 14:29:06 -0700

    Set-up for DD w/ CS '21. Mind versions.

---
+++ 20250310_CSDD.R
@@ -0,0 +1,13 @@
+# Replicate Wolfers (2006) with techniques by Callaway and Sant'Anna (2021).
+
+# Packages
+
+# install.packages("did")
+
+library(tidyverse)
+library(did)
+
+# Data
+
+# Estimation
+
