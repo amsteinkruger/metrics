d645782cdee52243d12a2186125647e52df4e78f
amsteinkruger <amsteinkruger@gmail.com>
2025-05-30 00:47:59 -0700

    Through an initial visualization.

--- 20250521_4_Inference.R
+++ 20250521_4_Inference.R
@@ -14,20 +14,18 @@ set.seed(0112358)
 # Parameters
 
 n = 30
-run_vec = seq(1, 10)
-beta_vec = seq(0, 2, by = 0.50)
-rho_vec = seq(0, 0.99, by = 0.50)
+run_vec = seq(1, 100)
+beta_vec = seq(0, 0.99, by = 0.01)
+rho_vec = seq(0, 0.99, by = 0.01)
 
 value = 3.84
 
-coverage = matrix(NA, length(beta_vec), length(rho_vec)) # probably don't need this
+Z = matrix(1, n, 1)
 
-Z = matrix(1, n, 1) # What is this FOR? (Constants?)
-
-# Iterate
+# Simulation
 
 dat = 
-  expand.grid(run = run_vec, rho = rho_vec, b_2 = beta_vec) %>% # Base R # draw = draw, 
+  expand.grid(run = run_vec, rho = rho_vec, b_2 = beta_vec) %>% 
   as_tibble %>% 
   mutate(sd_2 = (1 - rho ^ 2) ^ (1 / 2),
          sig_2 = 1 + b_2 ^ 2 * (1 - rho ^ 2)) %>% 
@@ -46,10 +44,37 @@ dat =
          Data_Long = map2(X_1, X_2, ~ cbind(.x, .y, Z)),
          M_Short = map(Data_Short, ~ .x %>% crossprod %>% solve),
          M_Long = map(Data_Long, ~ .x %>% crossprod %>% solve),
-         Coefficients_Short = ,
-         Coefficients_Long = ,
-         )
+         Coefficients_Short = 
+           list(M_Short = M_Short, Data_Short = Data_Short, Y = Y) %>% 
+           pmap(function(M_Short, Data_Short, Y) M_Short %*% crossprod(Data_Short, Y)),
+         Coefficients_Long = 
+           list(M_Long = M_Long, Data_Long = Data_Long, Y = Y) %>% 
+           pmap(function(M_Long, Data_Long, Y) M_Long %*% crossprod(Data_Long, Y)),
+         V_Short = map2(M_Short, sig_2, ~ .x * .y),
+         V_Long = M_Long,
+         t_1_Short = map2(Coefficients_Short, V_Short, ~ .x[1] ^ 2 / .y[1, 1]),
+         t_1_Long = map2(Coefficients_Long, V_Long, ~ .x[1] ^ 2 / .y[1, 1]),
+         t_2_Long = map2(Coefficients_Long, V_Long, ~ .x[2] ^ 2 / .y[2, 2]),
+         t_Check = 
+           list(t_1_Short, t_1_Long, t_2_Long) %>% 
+           pmap(function(t_1_Short, t_1_Long, t_2_Long) ifelse(t_2_Long > value, t_1_Long, t_1_Short)),
+         Coverage = t_Check %>% map(~ .x < value))
+
+dat_less = 
+  dat %>% 
+  unnest(Coverage) %>% 
+  group_by(rho, b_2) %>% # Group to compute coverage probabilities.
+  summarize(Probability = mean(Coverage)) %>% 
+  ungroup
 
 # Tabulate
 
 # Visualize
+
+vis = 
+  dat_less %>% 
+  ggplot() +
+  geom_raster(aes(x = rho,
+                  y = b_2,
+                  fill = Probability)) + 
+  scale_fill_viridis_c(option = "H", direction = -1)
